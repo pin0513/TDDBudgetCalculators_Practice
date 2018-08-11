9fb827584dc5b77d85927ad9e682470b3a0a0d2a
paul_iphuang <paul_iphunag@tutorabc.com>
2018-08-11 22:53:42 +0800

    add no budget test and pass the test define api

---
+++ TDDBudgetCalculator01/BudgetCalc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDDBudgetCalculator01
+{
+    public class BudgetCalc
+    {
+        public decimal TotalAmount(DateTime start, DateTime end)
+        {
+            return 0;
+        }
+    }
+}
---
+++ TDDBudgetCalculator01/BudgetCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TDDBudgetCalculator01
+{
+    /// <summary>
+    /// BudgetCalculatorTests 的摘要說明
+    /// </summary>
+    [TestClass]
+    public class BudgetCalculatorTests
+    {
+        [TestMethod]
+        public void NoBudgets()
+        {
+            var budgetCalc = new BudgetCalc();
+            var start = new DateTime(2018, 03, 01);
+            var end = new DateTime(2018, 03, 01);
+            var amount = budgetCalc.TotalAmount(start, end);
+            Assert.AreEqual(0, amount);
+        }
+    }
+}
